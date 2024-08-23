4eb857dbefbdb0558aa7cfde4a60f8ffadb059a5
Nelson_Muquissi <emelsonmuquissi@gmail.com>
2024-08-23 16:05:53 -0300

    Finalizando as telas para crud dos contactos

--- Controllers/ContactoController.js
+++ Controllers/ContactoController.js
@@ -2,17 +2,41 @@ class ContactoController {
   index(req, res) {
     // const usuario = req.session.usuario
     const usuario = req.session.usuario;
-    const params = { usuario: usuario };
+    const contacto = usuario.contacto
+    const params = { usuario: usuario, contacto: contacto };
     res.render("./contacto/", params);
   }
 
-  show(req, res) {}
+  show(req, res) {
+     const id = req.params.id;
+     const contacto = req.session.usuario.contacto[id];
+     const params = { contacto: contacto, id: id };
+     res.render("contacto/show", params);
+  }
 
-  store(req, res) {}
+  store(req, res) {
+    const contacto = req.body.contacto
+    const usuario = req.session.usuario
 
-  update(req, res) {}
+    usuario.contacto.push(contacto)
 
-  delete(req, res) {}
+    res.redirect('/contacto')
+  }
+
+  update(req, res) {
+    const id = req.params.id
+    const usuario = session.usuario
+    const contacto = usuario.contacto[id]
+    const params = {usuario: usuario, contacto: contacto, id: id}
+    res.render("contacto/update", params);
+  }
+
+  delete(req, res) {
+    const usuario = req.session.usuario
+    const id = req.params.id
+    usuario.contacto.splice(id,1)
+    res.redirect("/contacto");
+  }
 }
 
 export default new ContactoController()
