71eb87d3f52b51bba2f061013598ca54e6a00b6d
Nelson_Muquissi <emelsonmuquissi@gmail.com>
2024-08-23 15:20:47 -0300

    Criando funcoes no controller para realizar o crud

--- Controllers/ContactoController.js
+++ Controllers/ContactoController.js
@@ -1,12 +1,18 @@
-class ContactoController{
+class ContactoController {
+  index(req, res) {
+    // const usuario = req.session.usuario
+    const usuario = req.session.usuario;
+    const params = { usuario: usuario };
+    res.render("./contacto/", params);
+  }
 
-    index(req, res){
-        // const usuario = req.session.usuario
-        const usuario = req.session.usuario
-        const params = {usuario: usuario}
-        res.render('./contacto/', params)
-    }
+  show(req, res) {}
 
+  store(req, res) {}
+
+  update(req, res) {}
+
+  delete(req, res) {}
 }
 
 export default new ContactoController()
