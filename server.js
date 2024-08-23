3c2b82e8503196170d30ec61120a7e24f9c87034
Nelson_Muquissi <emelsonmuquissi@gmail.com>
2024-08-22 23:55:00 -0300

    Alteracao da mensagem de abertura

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, (error) => {
     if(error) return console.log({mensagem:{erro:error}})
-    console.log(`Servidor rodando em http://localhost:${PORT}`)
+    console.log(`NewClick disponivel em http://localhost:${PORT}`)
 })
